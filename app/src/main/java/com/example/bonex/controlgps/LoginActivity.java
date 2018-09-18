419b67e3dfd7e73b268b1f83b0c28dd8456665e7
HuesoMarkic <andresmarkic@gmail.com>
2018-09-18 19:05:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/bonex/controlgps/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.bonex.controlgps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+}
